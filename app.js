ac58ba23ab8664c6176aa4b520e9eac35a534c2b
Sushrut-Kane <142163622+Sushrut-Kane@users.noreply.github.com>
2024-09-21 10:41:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+// Navigation to About me page
+$(document).ready(function () {
+  const aboutButton = $(".about-button");
+
+  aboutButton.on("click", function () {
+    window.location.href = "about_me.html";
+  });
+});
+
+// Navigation to About me page
+$(document).ready(function () {
+  const aboutButton = $(".contact-button");
+
+  aboutButton.on("click", function () {
+    window.location.href = "contact.html";
+  });
+});
+
+// Navigation to About me page
+$(document).ready(function () {
+  const aboutButton = $(".projects-button");
+
+  aboutButton.on("click", function () {
+    window.location.href = "project.html";
+  });
+});
+
+// Navigation to About me page
+$(document).ready(function () {
+  const aboutButton = $(".skills-button");
+
+  aboutButton.on("click", function () {
+    window.location.href = "skills.html";
+  });
+});
